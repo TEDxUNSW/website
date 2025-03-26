82a422f02ce17655d3a2c90a0f3b86b11af3a571
Shay <126753181+Gama-Flomic11@users.noreply.github.com>
2025-03-26 19:30:22 +1100

    Add navbar with logos and text (#3)
    Co-authored-by: Shay Middleton <z5585720@ad.unsw.edu.au>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Handjet} from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${handjet.variable} antialiased`}
       >
+        <NavBar />
         {children}
       </body>
     </html>
