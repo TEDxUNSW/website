73f5594e8020f65e5a4f67635a169682f55c1d44
Shay <126753181+Gama-Flomic11@users.noreply.github.com>
2025-04-08 22:05:21 +1000

    Add navBar that is dynamic based on the screen size (#4)
    Co-authored-by: Shay Middleton <z5585720@ad.unsw.edu.au>

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -8,28 +8,23 @@ export default function NavBar({
   }) {
     return (
       <header>
-        <div className="flex justify-between items-center bg-white text-black">
-          <div className="w-50 flex-none ">
-            <Image src={logo} alt="TEDxUNSW logo"/>
-          </div>
-          <div className="w-160 flex justify-between">
-            <div className="w-130 flex justify-between items-center font-helvetica text-2xl">
-              <div className="flex-initial">Events</div>
-              <div className="flex-initial">About</div>
-              <div className="flex-initial">Subscribe</div>
-              <div className="flex-initial">Participate</div>
-              <div className="flex-initial">Tickets</div>
+        <div className="flex flex-row items-center bg-white text-black h-15">
+          <Image className="flex-8 shrink-0 grow-0" src={logo} alt="TEDxUNSW logo" width="130"/>
+          <div className="max-[42rem]:flex-20 min-[42rem]:flex-1 min-[43rem]:flex-2 min-[47rem]:flex-3 md:flex-4 lg:flex-5"></div>
+          <div className="flex flex-auto shrink-0 justify-between">
+            <div className="flex max-[37rem]:hidden flex-30 shrink-0 justify-between items-center font-helvetica text-1xl font-bold">
+              <div className="flex-none px-1">Events</div>
+              <div className="flex-none px-1">About</div>
+              <div className="flex-none px-1">Subscribe</div>
+              <div className="flex-none px-1">Participate</div>
+              <div className="flex-none px-1">Tickets</div>
             </div>
-            <div className="w-25 flex justify-between items-center font-helvetica text-2xl">
-              <div className="flex-initial">
-                <Image src={insta} alt="Our Instagram"/>
-              </div>
-              <div className="flex-initial">
-                <Image src={facebook} alt="Our Facebook"/>
-              </div>
-              <div className="flex-initial">
-                <Image src={hamburg} alt="Options"/>
-              </div>
+            <div className="max-[37rem]:hidden flex-1"></div>
+            <div className="flex flex-auto justify-between items-center font-helvetica text-2xl">
+                <Image src={insta} alt="Our Instagram" className="flex-none px-0.5 "/>
+                <Image src={facebook} alt="Our Facebook" className="flex-none px-0.5"/>
+                <Image src={hamburg} alt="Options" className="flex-none pr-3.0"/>
+                <div/>                
             </div>
           </div>
         </div>
