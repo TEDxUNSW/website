8b1baf2b976cafa905d02772138230c249a1b71a
Lucas <me@lhvy.dev>
2025-05-15 12:24:23 +1000

    Add WIP footer (#6)

---
+++ components/Footer.tsx
@@ -0,0 +1,46 @@
+import Wave from "../public/wave.svg";
+import Logo from "../public/logo-no-color.svg";
+
+export default function Footer() {
+  return (
+    <footer className="text-white relative overflow-hidden">
+      <div className="absolute z-0 w-full h-full bottom-0">
+        <div className="flex flex-col justify-end items-center w-full h-full">
+          <Wave className="object-cover w-[400%] h-auto" />
+          <div className="bg-[rgb(226,72,67)] h-50 w-full" />
+        </div>
+      </div>
+
+      <div className="relative z-10 pt-64 w-[75%] mx-auto pb-20">
+        <div className="w-[80%] mx-auto">
+          <h1 className="text-center text-8xl font-bold">TEDxUNSW</h1>
+          <div className="flex flex-row justify-center text-black">
+            <p className="text-4xl font-bold">
+              Sign up for email updates from TEDxUNSW
+            </p>
+            <div className="flex flex-row items-center">
+              <input
+                type="email"
+                placeholder="Your email address"
+                className="ml-4 p-2 underline underline-offset-10 decoration-black placeholder:text-black text-2xl text-bold"
+              />
+              <button className="ml-4 p-2 bg-black text-white">Submit</button>
+            </div>
+          </div>
+        </div>
+        <div className="text-black">
+          <hr className="mx-auto my-4 border-t-1 border-black" />
+          <div className="flex flex-row justify-between">
+            <div>
+              <Logo className="w-auto" />
+              <p>
+                This independent TEDx event is operated under license from TED.
+              </p>
+            </div>
+            <p className="text-2xl font-bold">2023</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
