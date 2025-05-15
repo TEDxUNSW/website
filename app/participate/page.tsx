227bf200d933823548390235a805d5914c07a519
Sky Lam <sky.l.lam@student.unsw.edu.au>
2025-05-15 14:12:58 +1000

    Added pages from navbar

---
+++ app/participate/page.tsx
@@ -0,0 +1,8 @@
+export default function participatePage() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold">About TEDxUNSW</h1>
+      <p className="mt-4">This is the about the participatePage content...</p>
+    </main>
+  )
+}
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,8 @@
-import Logo from "../public/logo-black-svg.svg";
-import Hamburg from "../public/hamburg.svg";
-import Facebook from "../public/facebook.svg";
-import Instagram from "../public/instagram.svg";
+import Logo from "../public/logo-black-svg.svg"
+import Hamburg from "../public/hamburg.svg"
+import Facebook from "../public/facebook.svg"
+import Instagram from "../public/instagram.svg"
+import Link from "next/link"
 
 export default function NavBar({}) {
   return (
@@ -10,22 +11,26 @@ export default function NavBar({}) {
         <Logo className="shrink-0 grow-0" width={180} />
         <div className="max-[42rem]:flex-20 min-[42rem]:flex-1 min-[43rem]:flex-2 min-[47rem]:flex-3 md:flex-4 lg:flex-5"></div>
         <div className="flex flex-auto shrink-0 justify-between">
-          <div className="flex max-[42rem]:hidden flex-30 shrink-0 justify-between items-center font-helvetica text-1xl font-bold">
-            <div className="flex-none px-1">Events</div>
-            <div className="flex-none px-1">About</div>
-            <div className="flex-none px-1">Subscribe</div>
-            <div className="flex-none px-1">Participate</div>
-            <div className="flex-none px-1">Tickets</div>
-          </div>
           <div className="max-[42rem]:hidden flex-1"></div>
-          <div className="flex flex-auto justify-between items-center font-helvetica text-2xl w-30 pl-2">
-            <Instagram className="shrink-0 grow-0" width={25} />
-            <Facebook className="shrink-0 grow-0" width={25} />
-            <Hamburg className="shrink-0 grow-0 pr-3.0" width={25} />
-            <div />
+          <div className="flex max-[42rem]:hidden flex-30 shrink-0 justify-between items-center font-helvetica text-1xl font-bold">
+            <Link href="/events" className="flex-none px-1">
+              Events
+            </Link>
+            <Link href="/about" className="flex-none px-1">
+              About
+            </Link>
+            <Link href="/subscribe" className="flex-none px-1">
+              Subscribe
+            </Link>
+            <Link href="/participate" className="flex-none px-1">
+              Participate
+            </Link>
+            <Link href="/tickets" className="flex-none px-1">
+              Tickets
+            </Link>
           </div>
         </div>
       </div>
     </header>
-  );
+  )
 }
