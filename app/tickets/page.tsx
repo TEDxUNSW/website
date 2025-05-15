ca939d36d0ece9a26640180c1b36721a704952e1
Sky828 <77192644+Sky828@users.noreply.github.com>
2025-05-15 14:48:02 +1000

    Added pages from navbar (#8)
    Co-authored-by: Sky Lam <sky.l.lam@student.unsw.edu.au>

---
+++ app/tickets/page.tsx
@@ -0,0 +1,8 @@
+export default function TicketsPage() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold">About TEDxUNSW</h1>
+      <p className="mt-4">This is the about the ticketsPage content...</p>
+    </main>
+  )
+}
