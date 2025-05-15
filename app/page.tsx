696c19f2fea4f5f00f0b4be11ea04f842f5f1a15
charlene liu <charleneliu05@outlook.com>
2025-05-15 12:54:50 +1000

    add talsk indicator

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-items-center min-h-screen  gap-16 sm: font-[family-name:var(--font-handjet)] text-5xl">
       In Plain Sight
       {/* mission&&talks recommadation session */}
-      <div className="flex bg-[url('/BlackBackground.png')]  bg-repeat bg-contain w-screen h-screen ">
+      <div className="flex flex-1 bg-[url('/BlackBackground.png')]  bg-repeat bg-contain w-screen h-screen ">
         <div className="p-8 px-12 flex-col flex items-start justify-between text-white gap-5 h-96 w-full font-[family-name:var(--font-geist-mono)] font-bold md:pr-52 md:px-20">
           <div className="flex gap-5 flex-col sm:flex-row md:gap-20">
             <Title
@@ -29,6 +29,7 @@ export default function Home() {
               color="white"
               route="/talks"
             />
+            {/* this is where talks are recommended */}
           </div>
         </div>
       </div>
