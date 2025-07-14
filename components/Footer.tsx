6a1930d1d34ce9f1408181e2c7ad7862d8183d5a
Lucas <me@lhvy.dev>
2025-07-14 10:06:58 +1000

    Hide footer wave (#22)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,38 +1,63 @@
 "use client";
 
-import Wave from "../public/wave.svg";
-import Logo from "../public/logo-no-color.svg";
 import { useState } from "react";
+import isEmail from "validator/es/lib/isEmail";
+import Logo from "../public/logo-no-color.svg";
+
+const WEBHOOK_URL =
+  process.env.NEXT_PUBLIC_WEBHOOK_URL ?? "https://tedxunsw.com.au/nowhere";
 
 export default function Footer() {
   const [email, setEmail] = useState<string>("");
 
+  const sendEmailToWebhook = async (email: string) => {
+    const toSend = email.trim();
+    if (!isEmail(toSend)) {
+      return;
+    }
+    setEmail("");
+
+    fetch(WEBHOOK_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: `New email subscription: ${email}`,
+      }),
+    });
+  };
+
   return (
-    <footer className="text-white relative overflow-hidden">
-      <div className="absolute z-0 w-full h-full bottom-0">
+    <footer className="text-white relative overflow-hidden bg-[rgb(226,72,67)]">
+      {/* <div className="absolute z-0 w-full h-full bottom-0">
         <div className="flex flex-col justify-end items-center w-full h-full">
           <Wave className="object-cover w-[400%] h-auto" />
           <div className="bg-[rgb(226,72,67)] h-[80%] w-full"></div>
         </div>
-      </div>
+      </div> */}
 
-      <div className="relative z-10 pt-64 w-[75%] mx-auto pb-20">
+      <div className="relative z-10 pt-10 w-[75%] mx-auto pb-10">
         <div className="w-[80%] mx-auto">
-          <h1 className="text-center text-8xl font-bold">TEDxUNSW</h1>
-          <div className="flex flex-row justify-center text-black">
-            <p className="text-4xl font-bold">
+          {/* <h1 className="text-center text-8xl font-bold">TEDxUNSW</h1> */}
+          <div className="flex flex-col md:flex-row justify-center text-black">
+            <p className="text-3xl font-bold text-center md:text-left">
               Sign up for email updates from TEDxUNSW
             </p>
-            <div className="flex flex-row items-center">
+            <div className="flex flex-row items-center mt-0">
               <input
                 type="email"
                 placeholder="Your email address"
-                className="ml-4 p-2 underline underline-offset-10 decoration-black placeholder:text-black text-2xl text-bold"
+                className="p-2 underline underline-offset-10 decoration-black placeholder:text-black text-2xl font-bold "
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-
               />
-              <button className="ml-4 p-2 bg-black text-white" onClick={() => setEmail("")}>Submit</button>
+              <button
+                className="mt-4 md:mt-0 md:ml-4 p-2 bg-black text-white"
+                onClick={() => sendEmailToWebhook(email)}
+              >
+                Submit
+              </button>
             </div>
           </div>
         </div>
@@ -45,7 +70,7 @@ export default function Footer() {
                 This independent TEDx event is operated under license from TED.
               </p>
             </div>
-            <p className="text-2xl font-bold">2023</p>
+            {/* <p className="text-2xl font-bold">2025</p> */}
           </div>
         </div>
       </div>
