de69b30832b8a74ec3d3e448d8f5398773e2c966
Lucas <me@lhvy.dev>
2025-09-22 11:58:55 +1000

    Scaffold Off The Record hero component

--- components/Event.tsx
+++ components/Event.tsx
@@ -1,9 +1,9 @@
-import SaveTheDate25T3 from "./Themes/25T3TBD";
+import OffTheRecord25T3 from "./Themes/25T3OffTheRecord";
 
 export default function Event() {
   return (
     <div className="flex flex-col items-center justify-items-center w-full">
-      <SaveTheDate25T3 />
+      <OffTheRecord25T3 />
       {/* Event details */}
     </div>
   );
---
+++ components/Themes/25T3OffTheRecord.tsx
@@ -0,0 +1,36 @@
+// import Link from "next/link";
+
+export default function OffTheRecord25T3() {
+  return (
+    <div
+      className="flex flex-col relative items-center justify-center w-full h-full lg:h-screen bg-[#0d0a03]"
+      style={{
+        fontFamily: "'Times New Roman', Times, serif",
+      }}
+    >
+      <div className="text-[#ffd144]">
+        <div>
+          <div>
+            <div
+              className="text-[12rem] font-tnr-condensed"
+              style={{
+                fontWeight: "bold",
+              }}
+            >
+              OFF
+            </div>
+            <div className="text-[6rem] font-tnr-condensed">THE RECORD</div>
+          </div>
+          <div>
+            <p className="font-tnr-condensed">
+              we celebrate conversations that have slipped through the cracks;
+              voices unheard, tales untold, unsung heroes and lovers in the
+              shadows. we champion ideas that never made it into the spotlight,
+              stories lost to time, and the characters hiding in the margins.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
