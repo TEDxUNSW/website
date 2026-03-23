2456c14cf667267dc3fc5f8ff3936c40214ffa74
Ddrewy <deandrew.y@gmail.com>
2026-03-23 17:28:57 +1100

    update: updating the about page content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,56 @@ export default function AboutPage() {
   return (
     <main className="p-10">
       <h1 className="text-4xl font-bold">About TEDxUNSW</h1>
-      <p className="mt-4">This is the about page content...</p>
+
+      <p className="mt-4">In the spirit of discovering and spreading ideas, 
+        TEDx is a program of local, self-organized events that bring people together to share a 
+        TED-like experience. At a TEDx event, TED Talks video and live speakers combine to spark 
+        deep discussion and connection. These local, self-organized events are branded TEDx, 
+        where x = independently organized TED event. The TED Conference provides general guidance 
+        for the TEDx program, but individual TEDx events are self-organized. 
+        (Subject to certain rules and regulations.)
+      </p>
+      <br/>
+      <h1 className="text-4xl font-bold">About TED</h1>
+      <p className="mt-4">
+        TED is a nonprofit, nonpartisan organization dedicated to discovering, debating and 
+        spreading ideas that spark conversation, deepen understanding and drive meaningful change. 
+        Our organization is devoted to curiosity, reason, wonder and the pursuit of 
+        knowledge — without an agenda. We welcome people from every discipline and culture who seek 
+        a deeper understanding of the world and connection with others, and we invite everyone to 
+        engage with ideas and activate them in your community.
+      </p>
+      <br/>
+      <p className="mt-4">
+        TED began in 1984 as a conference where Technology, Entertainment and Design converged, 
+        but today it spans a multitude of worldwide communities and initiatives exploring everything 
+        from science and business to education, arts and global issues. In addition to the TED Talks 
+        curated from our annual conferences and published on TED.com, we produce
+        <a href="https://audiocollective.ted.com/"className="underline ml-1">original podcast</a>,  
+        <a href="https://www.ted.com/series" className="underline ml-1">short video series</a>,  
+        <a href="https://ed.ted.com/" className="underline ml-1 mr-1">animated educational lessons (TED-Ed)</a> 
+        and TV programs that are translated into more than 100 languages and distributed via 
+        partnerships around the world. Each year, thousands of independently run 
+        <a href="https://www.ted.com/about/programs-initiatives/tedx-program" className="underline ml-1">TEDx events</a>. 
+        Through the <a href="https://www.audaciousproject.org/" className="underline ml-1">Audacious Project</a>, 
+        TED has helped catalyze $6.6 billion in funding for projects that support bold solutions to 
+        the world's most urgent challenges — working to make the world more beautiful, sustainable 
+        and just. In 2020, TED launched <a href="https://countdown.ted.com/" className="underline ml-1">Countdown</a>, 
+        an initiative to accelerate solutions to the climate crisis and mobilize a movement for a net-zero future, 
+        and in 2023 TED launched 
+        <a href="https://www.ted.com/about/programs-initiatives/teddemocracy" className="underline ml-1 mr-1">TED Democracy</a> 
+        to spark a new kind of conversation focused on realistic pathways towards a 
+        more vibrant and equitable future. View a full list of 
+        <a href="https://www.ted.com/about/programs-initiatives" className="underline ml-1">TED’s many programs and initiatives</a>.
+      </p>
+      <br/>
+      <p className="mt-4">
+        Follow TED on <a href="https://www.facebook.com/TED" className="underline ml-1">Facebook</a>, 
+        <a href="https://www.instagram.com/ted" className="underline ml-1">Instagram</a>, 
+        <a href="https://www.linkedin.com/company/ted-conferences/" className="underline ml-1">LinkedIn</a>, 
+        <a href="https://www.tiktok.com/@tedtoks?lang=en" className="underline ml-1">TikTok</a>, and 
+        <a href="https://x.com/TEDTalks" className="underline ml-1">X</a>.
+      </p>
     </main>
   );
 }
