d8cadc07c345b85d78a334f9cc269a9e278f1521
Ddrewy <deandrew.y@gmail.com>
2026-04-05 16:17:38 +1000

    update: updating the prevEvents page, to be neater and might be used/merged to main events page

--- app/prevEvents/page.tsx
+++ app/prevEvents/page.tsx
@@ -1,13 +1,18 @@
 export default function prevEvents() {
     return (
-        <div className="max-h-screen gap-15 pl-10 pr-10 pt-5 pb-5 grid grid-cols-5 place-items-stretch justify-between">
-            <div className="border-transparent rounded-md w-full h-[20vw] p-4 shadow-md shadow-red-500 bg-background">Events 1</div>
-            <div className="border-transparent rounded-md w-full h-[20vw] p-4 shadow-md  shadow-red-500 bg-background">Events 2</div>
-            <div className="border-transparent rounded-md w-full h-[20vw] p-4 shadow-md  shadow-red-500 bg-background">Events 3</div>
-            <div className="border-transparent rounded-md w-full h-[20vw] p-4 shadow-md  shadow-red-500 bg-background">Events 4</div>
-            <div className="border-transparent rounded-md w-full h-[20vw] p-4 shadow-md  shadow-red-500 bg-background">Events 5</div>
-            <div className="border-transparent rounded-md w-full h-[20vw] p-4 shadow-md  shadow-red-500 bg-background">Events 6</div>
-            <div className="border-transparent rounded-md w-full h-[20vw] p-4 shadow-md  shadow-red-500 bg-background">Events 7</div>
+        <div className="h-screen pl-10 pr-10 pt-5 pb-5 place-items-stretch justify-between">
+            <h1 className="font-bold text-3xl mb-3">2026</h1>
+            <div className="gap-15 grid grid-cols-3">
+                <div className="border-transparent rounded-md w-full h-[20vw] p-4 shadow-md shadow-red-500 bg-background">Events 26T1</div>
+                <div className="border-transparent rounded-md w-full h-[20vw] p-4 shadow-md  shadow-red-500 bg-background">Events 26T2</div>
+                <div className="border-transparent rounded-md w-full h-[20vw] p-4 shadow-md  shadow-red-500 bg-background">Events 26T3</div>
+            </div>
+            <h1 className="font-bold text-3xl mb-3 mt-5 sticky right-0">2025</h1>
+            <div className="gap-15 grid grid-cols-3">
+            <div className="border-transparent rounded-md w-full h-[20vw] p-4 shadow-md  shadow-red-500 bg-background">Events 25T1</div>
+            <div className="border-transparent rounded-md w-full h-[20vw] p-4 shadow-md  shadow-red-500 bg-background">Events 25T2</div>
+            <div className="border-transparent rounded-md w-full h-[20vw] p-4 shadow-md  shadow-red-500 bg-background">Events 25T3</div>
+            </div>
         </div>
     )
 }
