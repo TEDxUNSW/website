221c2b349ac9c507c05961da44ac2ae39a499a38
Sophie <sophie@foxy.net.au>
2026-06-07 21:51:29 +1000

    About page (#37)
    Authored-by: fox-58

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,10 +7,17 @@ import Link from "next/link";
 export default function NavBar({}) {
   return (
     <header>
-      <div className="flex flex-row items-center justify-between bg-white text-black h-15 w-full px-4">
-        <Link href="/">
-          <Logo className="shrink-0 grow-0" width={180} />
-        </Link>
+      <div className="flex flex-row items-center justify-between bg-white text-black h-15 w-full pr-2">
+        <div className="flex flex-row h-15">
+          <Link href="/">
+            <Logo className="shrink-0 grow-0 h-15" width={180} />
+          </Link>
+          <Link href="/about" className="flex-0 pt-5 pb-5 pl-2 z-10">
+            <div className="font-helvetica text-[18px] font-bold ">
+              <p>About</p>
+            </div>
+          </Link>
+        </div>
         <div className="flex items-center gap-4">
           <a
             href="https://www.instagram.com/tedxunsw/"
@@ -32,12 +39,11 @@ export default function NavBar({}) {
           <div className="flex max-[42rem]:hidden flex-30 shrink-0 justify-between items-center font-helvetica text-1xl font-bold">
             <Link href="/events" className="flex-none px-1">
               Events
-            </Link>
-            <Link href="/about" className="flex-none px-1">
-              About
-            </Link>
+            </Link> 
+        */}
+        {/*
             <Link href="/subscribe" className="flex-none px-1">
-              Subscribe
+              Subscribe 
             </Link>
             <Link href="/participate" className="flex-none px-1">
               Participate
