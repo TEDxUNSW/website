2bb1e333a6c6960c1651c6264cba3431132437ab
Ddrewy <deandrew.y@gmail.com>
2026-07-27 18:58:34 +1000

    update: update the className to have mx-auto

--- components/Upcoming.tsx
+++ components/Upcoming.tsx
@@ -4,10 +4,10 @@ import { StarsBackground } from "./animate-ui/components/backgrounds/stars";
 export default function Upcoming() {
   return (
     <div className="bg-none">
-      <div className="flex bg-none h-[100vh] w-screen items-center justify-center">
+      <div className="flex bg-none h-[100vh] w-screen">
         <StarsBackground className="z-[0] absolute" />
-        <div className="flex z-2 items-center justify-center bg-yellow-600 border-transparent rounded-2xl w-[35vw] h-[40vh] shadow-xl shadow-red-700">
-          <div className="flex items-center justify-center bg-stone-700 border-transparent rounded-2xl w-[32vw] h-[35vh]">
+        <div className="flex z-2 items-center justify-center mx-auto bg-yellow-600 border-transparent rounded-2xl w-[35vw] h-[40vh] shadow-xl shadow-red-700">
+          <div className="flex items-center justify-center mx-auto bg-stone-700 border-transparent rounded-2xl w-[32vw] h-[35vh]">
             <h1 className="text-3xl font-bold text-white">
               Under construction
             </h1>
