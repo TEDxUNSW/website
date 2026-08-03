aedf16d9babcf61f2d9dfafc44fb4f37e9302ab2
Ddrewy <deandrew.y@gmail.com>
2026-08-03 18:42:15 +1000

    update: updating the FE to fix the issue in PR comment

--- components/Upcoming.tsx
+++ components/Upcoming.tsx
@@ -1,13 +1,22 @@
 // component to show that the route is currently under construction
 import { StarsBackground } from "./animate-ui/components/backgrounds/stars";
+const centerStyle = "flex items-center justify-center mx-auto";
 
 export default function Upcoming() {
   return (
-    <div className="bg-none">
-      <div className="flex bg-none h-[100vh] w-screen">
-        <StarsBackground className="z-[0] absolute" />
-        <div className="flex z-2 items-center justify-center mx-auto bg-yellow-600 border-transparent rounded-2xl w-[35vw] h-[40vh] shadow-xl shadow-red-700">
-          <div className="flex items-center justify-center mx-auto bg-stone-700 border-transparent rounded-2xl w-[32vw] h-[35vh]">
+    <div className="relative flex bg-none h-screen w-screen overflow-hidden">
+      <StarsBackground className="z-0 absolute inset-0" />
+
+      {/* Centering the block*/}
+      <div className={`${centerStyle} z-10 w-full h-full`}>
+        {/* the big card */}
+        <div
+          className={`${centerStyle} bg-yellow-600 border-transparent rounded-2xl w-[35vw] h-[40vh] shadow-xl shadow-red-700`}
+        >
+          {/* smaller inner card */}
+          <div
+            className={`${centerStyle} bg-stone-700 border-transparent rounded-2xl w-[32vw] h-[35vh]`}
+          >
             <h1 className="text-3xl font-bold text-white">
               Under construction
             </h1>
