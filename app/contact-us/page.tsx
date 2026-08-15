9f8e522330b4a1889b1d72c5feb86b53d5bd05c9
z5585720 <z5585720@ad.unsw.edu.au>
2026-08-15 19:09:35 +1000

    Fix contact us page for safari

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -5,23 +5,26 @@ import "./Contact.css";
 export default function ContactUsPage() {
   return (
     <>
-      <div className="justify-items-center">
-        <h1 className="text-3xl font-bold p-3 md:mx-30 max-md:mx-10">
-          Contact Us
-        </h1>
-        <div className="grid grid-cols-2 max-md:grid-cols-1 gap-4 w-1vw md:mx-20 max-md:mx-10 lg:w-220 ">
-          <div className="relative flex-1 h-85 max-[350px]:h-70 min-[350px]:max-sm:h-105 xs:h-110 md:h-95 bg-white text-black m-2 rounded-lg p-2 border-proper-red border-2 justify-items-center text-center">
-            <div className="relative w-99/100 h-full justify-items-center">
+      <div className="">
+        <h1 className="text-3xl font-bold mx-auto text-white px-5">
+            Contact Us
+          </h1>
+        <div className="grid grid-cols-2 max-md:grid-cols-1 gap-4 w-1vw mx-auto lg:w-220">
+          <div className="relative h-85 max-[350px]:h-70 min-[350px]:max-sm:h-105 xs:h-110 md:h-95 bg-white text-black m-2 rounded-lg p-2 border-proper-red border-2  text-center">
+            <div className="relative w-99/100 h-full">
               <h2 className="text-2xl font-bold h-15">
                 Nominations For Speakers
               </h2>
+              <div className=" w-full">
+              <div className="relative w-[170px] bg-red-100 h-full"></div>
               <Image
                 src="/logos/speak.svg"
                 alt="TEDxUNSW"
                 width={100}
                 height={500}
-                className="max-[270px]:hidden"
+                className="max-[270px]:hidden mx-auto"
               />
+              </div>
               <p className="mb-5 md:max-lg:mt-2 sm:max-md:mt-0 sm:max-md:mb-0 mt-5 max-[350px]:hidden">
                 If you are know someone who would be a good speaker for a future
                 TEDxUNSW events, send an email to us
@@ -45,8 +48,8 @@ export default function ContactUsPage() {
               </button>
             </div>
           </div>
-          <div className="relative flex-1 h-80 max-[350px]:h-70 min-[350px]:max-sm:h-105 xs:h-110 md:h-95 bg-white text-black m-2 rounded-lg p-2 border-proper-red border-2 justify-items-center text-center">
-            <div className="relative w-99/100 h-full justify-items-center">
+          <div className="relative h-80 max-[350px]:h-70 min-[350px]:max-sm:h-105 xs:h-110 md:h-95 bg-white text-black m-2 rounded-lg p-2 border-proper-red border-2  text-center">
+            <div className="relative w-99/100 h-full ">
               <h2 className="text-2xl font-bold h-15">
                 Looking to Sponsor an Event?
               </h2>
@@ -55,7 +58,7 @@ export default function ContactUsPage() {
                 alt="TEDxUNSW"
                 width={100}
                 height={500}
-                className="max-[270px]:hidden"
+                className="max-[270px]:hidden mx-auto"
               />
               <p className="mb-5 md:max-lg:mt-2 sm:max-md:mt-0 sm:max-md:mb-0 mt-5 max-[350px]:hidden">
                 Looking to sponsor a TEDxUNSW event? Please email us
@@ -81,8 +84,8 @@ export default function ContactUsPage() {
               </button>
             </div>
           </div>
-          <div className="relative flex-1 h-80 max-[350px]:h-70 min-[350px]:max-sm:h-105 xs:h-110 md:h-95 bg-white text-black m-2 rounded-lg p-2 border-proper-red border-2 justify-items-center text-center">
-            <div className="relative w-99/100 h-full justify-items-center">
+          <div className="relative h-80 max-[350px]:h-70 min-[350px]:max-sm:h-105 xs:h-110 md:h-95 bg-white text-black m-2 rounded-lg p-2 border-proper-red border-2  text-center">
+            <div className="relative w-99/100 h-full ">
               <h2 className="text-2xl font-bold h-15">
                 Cross Society Collaboration
               </h2>
@@ -91,7 +94,7 @@ export default function ContactUsPage() {
                 alt="TEDxUNSW"
                 width={100}
                 height={500}
-                className="max-[270px]:hidden"
+                className="max-[270px]:hidden mx-auto"
               />
               <p className="mb-5 md:max-lg:mt-2 sm:max-md:mt-0 sm:max-md:mb-0 mt-5 max-[350px]:hidden">
                 If you are a UNSW student society looking for an opportunity for
@@ -118,8 +121,8 @@ export default function ContactUsPage() {
               </button>
             </div>
           </div>
-          <div className="relative flex-1 h-80 max-[350px]:h-70 min-[350px]:max-sm:h-105 xs:h-110 md:h-95 bg-white text-black m-2 rounded-lg p-2 border-proper-red border-2 justify-items-center text-center">
-            <div className="relative w-99/100 h-full   justify-items-center">
+          <div className="relative  h-80 max-[350px]:h-70 min-[350px]:max-sm:h-105 xs:h-110 md:h-95 bg-white text-black m-2 rounded-lg p-2 border-proper-red border-2  text-center">
+            <div className="relative w-99/100 h-full   ">
               <h2 className="text-2xl font-bold h-15">
                 Volunteering for Events
               </h2>
@@ -128,7 +131,7 @@ export default function ContactUsPage() {
                 alt="TEDxUNSW"
                 width={100}
                 height={500}
-                className="max-[270px]:hidden"
+                className="max-[270px]:hidden mx-auto"
               />
               <p className="mb-5 md:max-lg:mt-2 sm:max-md:mt-0 sm:max-md:mb-0 mt-5 max-[350px]:hidden">
                 Are you a performer, a photographer or do you have skills useful
@@ -154,8 +157,8 @@ export default function ContactUsPage() {
               </button>
             </div>
           </div>
-          <div className="relative flex-1 h-80 max-[350px]:h-70 min-[350px]:max-sm:h-105 xs:h-110 md:h-95 bg-white text-black m-2 rounded-lg p-2 border-proper-red border-2 justify-items-center text-center">
-            <div className="relative w-99/100 h-full justify-items-center">
+          <div className="relative h-80 max-[350px]:h-70 min-[350px]:max-sm:h-105 xs:h-110 md:h-95 bg-white text-black m-2 rounded-lg p-2 border-proper-red border-2  text-center">
+            <div className="relative w-99/100 h-full ">
               <h2 className="text-2xl font-bold h-15">
                 Looking to Join the Society?
               </h2>
@@ -164,7 +167,7 @@ export default function ContactUsPage() {
                 alt="TEDxUNSW"
                 width={100}
                 height={500}
-                className="max-[270px]:hidden"
+                className="max-[270px]:hidden mx-auto"
               />
               <p className="mb-5 md:max-lg:mt-2 sm:max-md:mt-0 sm:max-md:mb-0 mt-5 max-[350px]:hidden">
                 Applications are not currently open, please check back at the
@@ -190,15 +193,15 @@ export default function ContactUsPage() {
               </button>
             </div>
           </div>
-          <div className="relative flex-1 h-80 max-[350px]:h-70  max-[768px]:h-100 min-[350px]:max-sm:h-105 xs:h-110 md:h-95 bg-white text-black m-2 rounded-lg p-2 border-proper-red border-2 justify-items-center text-center mb-10">
-            <div className="relative w-99/100 h-full   justify-items-center">
+          <div className="relative h-80 max-[350px]:h-70  max-[768px]:h-100 min-[350px]:max-sm:h-105 xs:h-110 md:h-95 bg-white text-black m-2 rounded-lg p-2 border-proper-red border-2  text-center mb-10">
+            <div className="relative w-99/100 h-full">
               <h2 className="text-2xl font-bold h-15">Copy Emails</h2>
               <Image
                 src="/logos/mail.svg"
                 alt="TEDxUNSW"
                 width={100}
                 height={500}
-                className="max-[270px]:hidden"
+                className="max-[350px]:hidden mx-auto"
               />
               <button
                 onClick={() => {
