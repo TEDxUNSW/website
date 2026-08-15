7b5b1b1ea906f331f4efa2889a0b77fd93b07854
z5585720 <z5585720@ad.unsw.edu.au>
2026-08-15 19:12:20 +1000

    Fix the linting

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -7,8 +7,8 @@ export default function ContactUsPage() {
     <>
       <div className="">
         <h1 className="text-3xl font-bold mx-auto text-white px-5">
-            Contact Us
-          </h1>
+          Contact Us
+        </h1>
         <div className="grid grid-cols-2 max-md:grid-cols-1 gap-4 w-1vw mx-auto lg:w-220">
           <div className="relative h-85 max-[350px]:h-70 min-[350px]:max-sm:h-105 xs:h-110 md:h-95 bg-white text-black m-2 rounded-lg p-2 border-proper-red border-2  text-center">
             <div className="relative w-99/100 h-full">
@@ -16,14 +16,14 @@ export default function ContactUsPage() {
                 Nominations For Speakers
               </h2>
               <div className=" w-full">
-              <div className="relative w-[170px] bg-red-100 h-full"></div>
-              <Image
-                src="/logos/speak.svg"
-                alt="TEDxUNSW"
-                width={100}
-                height={500}
-                className="max-[270px]:hidden mx-auto"
-              />
+                <div className="relative w-[170px] bg-red-100 h-full"></div>
+                <Image
+                  src="/logos/speak.svg"
+                  alt="TEDxUNSW"
+                  width={100}
+                  height={500}
+                  className="max-[270px]:hidden mx-auto"
+                />
               </div>
               <p className="mb-5 md:max-lg:mt-2 sm:max-md:mt-0 sm:max-md:mb-0 mt-5 max-[350px]:hidden">
                 If you are know someone who would be a good speaker for a future
