f710573981978bc507895885dbbbeb2dd85dc79d
Ddrewy <deandrew.y@gmail.com>
2026-09-14 17:21:11 +1000

    style: updating the style of text to scale automatically with vw

--- components/Upcoming.tsx
+++ components/Upcoming.tsx
@@ -19,7 +19,7 @@ export default function Upcoming() {
             className={`${centerStyle} bg-stone-700 border-transparent rounded-2xl 
             min-[329px]:w-[45vw] min-[329px]:h-[15vh] min-[414px]:w-[45vw] min-[711px]:w-[40vw] min-[711px]:h-[20vh] min-[890px]:w-[35vw] min[890px]:h-[25vh] min-[911px]:w-[40vw] min-[959px]:w-[50vw] @min-height-720px:h-[28vh] min-[1280px]:h-[35vh] min-[1280px]:w-[35vw]`}
           >
-            <h1 className="text-[15px] min-[330px]:text-md min-[414px]:text-lg min-[712px]:text-xl min-[800px]:text-2xl min-[911px]:text-3xl min-[1024px]:text-4xl font-bold text-white">
+            <h1 className="text-[3vw] font-bold text-white">
               Under construction
             </h1>
           </div>
