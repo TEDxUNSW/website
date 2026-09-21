ebf9eeec7df700c3a9a727137e942cf471c9f10b
Ddrewy <deandrew.y@gmail.com>
2026-09-21 17:05:25 +1000

    update: switching branch for upcomign so it's not clutered with events branch

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,8 +1,5 @@
-export default function EventsPage() {
-  return (
-    <main className="p-10">
-      <h1 className="text-4xl font-bold">About TEDxUNSW</h1>
-      <p className="mt-4">This is the about the events page content...</p>
-    </main>
-  );
+import Upcoming from "@/components/Upcoming";
+
+export default function Events() {
+  return <Upcoming />;
 }
